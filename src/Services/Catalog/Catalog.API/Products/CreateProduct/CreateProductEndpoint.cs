a3c64731bd1afec9f3ad2a56f365d6aeaeee1f38
adrsalomo <adryanslm.t@gmail.com>
2025-08-30 20:45:16 +0700

    SLO add request life cycle

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
@@ -1,7 +1,35 @@
 ﻿namespace Catalog.API.Products.CreateProduct;
 
+public record CreateProductRequest(string Name, List<string> Category, string Description,
+    string ImageFile, decimal Price);
 
+public record CreateProductResponse(Guid Id);
 
-public class CreateProductEndpoint
+public class CreateProductEndpoint : ICarterModule
 {
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPost("/products", async (CreateProductRequest request, ISender sender) =>
+        {
+            //make command object with MediatR to trigger command handler
+            var command = request.Adapt<CreateProductCommand>();
+
+            //send the command
+            var result = await sender.Send(command);
+
+            //get the response
+            var response = result.Adapt<CreateProductResponse>();
+
+            return Results.Created("/prducts/" + response.Id, response);
+
+
+
+
+        })
+        .WithName("CreateProduct")
+        .Produces<CreateProductResponse>(StatusCodes.Status201Created)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Create Product")
+        .WithDescription("Create Product");
+    }
 }
