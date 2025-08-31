4318b09163b1c5096bd2d08f8eaeb0dde10796cd
adrsalomo <adryanslm.t@gmail.com>
2025-08-31 14:57:29 +0700

    SLO add postgre DB

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,7 +1,4 @@
-﻿using BuildingBlocks.CQRS;
-using Catalog.API.Models;
-
-namespace Catalog.API.Products.CreateProduct;
+﻿namespace Catalog.API.Products.CreateProduct;
 
 public record CreateProductCommand(string Name, List<string> Category, string Description, 
     string ImageFile, decimal Price)
@@ -9,7 +6,7 @@ public record CreateProductCommand(string Name, List<string> Category, string De
 
 public record CreateProductResult(Guid Id);
 
-internal class CreateProductCommandHandler 
+internal class CreateProductCommandHandler (IDocumentSession session)
     : ICommandHandler<CreateProductCommand, CreateProductResult>
 {
     public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
@@ -27,14 +24,11 @@ internal class CreateProductCommandHandler
         };
 
         //save to database
+        session.Store(product);
+        await session.SaveChangesAsync(cancellationToken);
 
         //return CreateProductResult result
-        return new CreateProductResult(Guid.NewGuid());
+        return new CreateProductResult(product.Id);
 
-
-
-
-
-        throw new NotImplementedException();
     }
 }
